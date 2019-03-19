3cc16f1ca4a27d6ce7c9012135e7a2ac8c6548f8
Thomas Edwards <thce28@gmail.com>
2019-03-19 14:54:22 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+Author.create(name: 'Tomo', genre: "thriller", bio: "bio bants")
+
+Post.create(title: "the old maid", description: "description", author_id: 1)
